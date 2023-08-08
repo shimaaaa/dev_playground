2214e1710fa54fdb0f38e6b6f14a6223c1b66ef8
shinomiya <42191579+shimaaaa@users.noreply.github.com>
2023-08-08 23:47:48 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/ping")
+def ping() -> str:
+    return "pong"
+
+
 @app.get("/api")
 def read_root() -> dict:
     return {"Hello": "World"}
